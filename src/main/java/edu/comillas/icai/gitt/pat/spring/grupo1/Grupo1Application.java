472fa26d1a2891f8147fa779121bb35ae13818cb
patriciaurq <p.urquijo1@gmail.com>
2026-02-08 19:36:54 +0100

    actualizaciones del proyecto

--- src/main/java/edu/comillas/icai/gitt/pat/spring/grupo1/Grupo1Application.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/grupo1/Grupo1Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Grupo1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Grupo1Application.class, args);
 	}
-
 }
